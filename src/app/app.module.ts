47d3b73b49d0a2a6adb37bd5ab9837f5dec2504e
busraglk <busragelik@gmail.com>
2021-03-15 19:53:23 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -10,7 +10,10 @@ import { BrandService } from 'src/app/services/brand.service';
 export class BrandComponent implements OnInit {
 
   brands: Brand[] = [];
+  currentBrand: Brand;
   dataLoaded = false;
+
+
   constructor(private brandService: BrandService) { }
 
   ngOnInit(): void {
@@ -22,6 +25,19 @@ export class BrandComponent implements OnInit {
       this.brands = response.data;
       this.dataLoaded = true;
     })
+  }
+
+  setCurrentBrand(brand: Brand){
+   this.currentBrand = brand;
+  }
+
+  getCurrentBrandClass(brand: Brand){
+    if(brand == this.currentBrand) {
+      return "list-group-item active"
+    }
+    else {
+      return "list-group-item"
+    }
   }
 
 }
